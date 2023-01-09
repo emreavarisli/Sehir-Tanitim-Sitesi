be4db4c19581c990a1c6316794bc9878d0e03f67
Hasan Emre AVARİSLİ <81103889+emreavarisli@users.noreply.github.com>
2023-01-09 13:16:34 +0300

    Contact Page Add

--- script2.js
+++ script2.js
@@ -4,6 +4,7 @@ const two = document.querySelector('.two')
 const three = document.querySelector('.three')
 const four = document.querySelector('.four')
 const five = document.querySelector('.five')
+const six = document.querySelector('.six')
 var time = 0
 
 document.addEventListener('DOMContentLoaded', timer);
@@ -29,3 +30,20 @@ four.addEventListener('click',function(){
 five.addEventListener('click',function(){
     alert(time+' Saniye zaman geçirdiniz...')
 })
+six.addEventListener('click',function(){
+    alert(time+' Saniye zaman geçirdiniz...')
+})
+
+const text = document.querySelector('#text')
+const email = document.querySelector('#email')
+const konu = document.querySelector('#konu')
+const message = document.querySelector('#message')
+const submit = document.querySelector('#submit')
+
+submit.addEventListener('click',function(){
+    alert(`AD SOYAD: ${text.value}
+    EMAİL: ${email.value}
+    KONU: ${konu.value}
+    MESSAGE: ${message.value}`)
+})
+
